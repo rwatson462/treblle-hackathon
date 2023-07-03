2864f4ec06010510bbfc90ce827c83fa38171958
Rob Watson <rob.watson@me.com>
2023-07-03 23:00:53 +0100

    Update route name to include version

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('v1')->group(base_path('routes/v1.php'));
+Route::prefix('v1')->as('v1.')->group(base_path('routes/v1.php'));
 
 Route::prefix('/')->group(base_path('routes/fallback.php'));
--- tests/Feature/v1/Auth/RegisterTest.php
+++ tests/Feature/v1/Auth/RegisterTest.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Tests\Feature\Auth;
+namespace Tests\Feature\v1\Auth;
 
 use Tests\TestCase;
 
@@ -8,7 +8,7 @@ class RegisterTest extends TestCase
 {
     public function test_canRegisterUser(): void
     {
-        $response = $this->post(route('auth.register'), [
+        $response = $this->post(route('v1.auth.register'), [
             'email' => 'user@example.com',
             'name' => 'Test user',
             'password' => 'password',
