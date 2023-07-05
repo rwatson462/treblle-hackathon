0f4ab393e7c21fd86522f355ecca454104c766ba
Rob Watson <rob.watson@me.com>
2023-07-05 21:12:22 +0100

    Add GZIP response encoding

--- app/Http/Middleware/JsonMiddleware.php
+++ app/Http/Middleware/JsonMiddleware.php
@@ -25,6 +25,7 @@ class JsonMiddleware
         $response->headers->set('X-Frame-Options', 'deny');
         $response->headers->set('Content-Security-Policy', "default-src 'self'");
         $response->headers->remove('x-powered-by');
+        $response->headers->set('Content-encoding', 'gzip');
 
         return $response;
     }
